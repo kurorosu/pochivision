139747ad798a11b54c2744952ee3445a1dad1a70
kurorosu <149222114+kurorosu@users.noreply.github.com>
2025-04-22 19:34:18 +0900

    Add motion blur processor and validator; update config and documentation

--- processors/__init__.py
+++ processors/__init__.py
@@ -1,4 +1,4 @@
 from .base import BaseProcessor
 from .grayscale import GrayscaleProcessor
-from .blur import GaussianBlurProcessor, AverageBlurProcessor, MedianBlurProcessor, BilateralFilterProcessor
+from .blur import GaussianBlurProcessor, AverageBlurProcessor, MedianBlurProcessor, BilateralFilterProcessor, MotionBlurProcessor
 from .binarization import StandardBinarizationProcessor
