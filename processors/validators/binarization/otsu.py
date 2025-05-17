b8c66a13adf9974e9c68b22cae23788192355d79
kurorosu <kuronobu0903@gmail.com>
2025-05-17 18:32:54 +0900

    feat: Refactor binarization processors and validators for consistent design

--- processors/validators/binarization/otsu.py
+++ processors/validators/binarization/otsu.py
@@ -1,8 +1,10 @@
 """大津の2値化バリデータの実装モジュール."""
 
-from typing import Dict
+from typing import Any, Dict
 
-# from exceptions import ProcessorValidationError
+import numpy as np
+
+from exceptions import ProcessorValidationError
 from processors.validators.base import BaseValidator
 
 
@@ -11,40 +13,51 @@ class OtsuBinarizationValidator(BaseValidator):
     大津の2値化用のバリデータ.
 
     Args:
-        config (dict): バリデーション対象の設定辞書.
+        config (Dict[str, Any]): バリデーション対象の設定辞書.
 
     Raises:
         ProcessorValidationError: 不正なパラメータが検出された場合.
     """
 
-    def __init__(self, config: Dict[str, int]) -> None:
+    def __init__(self, config: Dict[str, Any]) -> None:
         """
         OtsuBinarizationValidatorのコンストラクタ.
 
         Args:
-            config (dict): バリデーション対象の設定辞書.
+            config (Dict[str, Any]): バリデーション対象の設定辞書.
         """
         self.config = config
 
     def validate(self) -> None:
         """
+        設定パラメータのバリデーションを実行します.
+
+        BaseValidatorの抽象メソッドを満たすために定義されています.
+        """
+        self.validate_config()
+
+    def validate_config(self) -> None:
+        """
         設定値のバリデーションを実行する.
 
-        Raises:
-            ProcessorValidationError: 不正なパラメータが検出された場合.
+        大津の2値化は設定パラメータを持たないため、このメソッドは何も行いません.
         """
-        # 共通バリデーション
-        # self.validate_image_type_and_nonempty(self.image) # processメソッドに移動
+        pass  # 設定パラメータに関するバリデーションは無い
 
-        # 2Dまたは3/4チャンネル画像のみ許可
-        # if not ( # processメソッドに移動
-        #     (self.image.ndim == 2)
-        #     or (self.image.ndim == 3 and self.image.shape[2] in (3, 4))
-        # ):
-        #     raise ProcessorValidationError(
-        #         "Input image must be 2D grayscale or "
-        #         "3/4 channel color image (BGR/BGRA)."
-        #     )
+    def validate_image(self, image: np.ndarray) -> None:
+        """
+        入力画像を検証します.
 
-        # 大津の2値化は特別な設定を必要としないため、追加のバリデーションは不要
-        pass  # 設定パラメータに関するバリデーションは無いが、メソッドは必要
+        Args:
+            image (np.ndarray): 入力画像.
+
+        Raises:
+            ProcessorValidationError: 入力画像が無効な場合.
+        """
+        self.validate_image_type_and_nonempty(image)
+
+        if not ((image.ndim == 2) or (image.ndim == 3 and image.shape[2] in (3, 4))):
+            raise ProcessorValidationError(
+                "Input image for OtsuBinarization must be 2D grayscale or "
+                "3/4 channel color image (BGR/BGRA)."
+            )
